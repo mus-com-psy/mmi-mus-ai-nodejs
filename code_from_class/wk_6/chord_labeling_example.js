c3ce13769c47519b3d9a44ab7fc33cbb0cb07318
Tom Collins <tomthecollins@gmail.com>
2024-02-20 21:18:24 -0500

    Chord labeling example included also.

--- code_from_class/wk_6/chord_labeling_example.js
+++ code_from_class/wk_6/chord_labeling_example.js
@@ -4,6 +4,8 @@ const ps = [
   [0, 45, 4], [0.5, 52, 3.5], [1, 59, 0.5], [1.5, 60, 2.5],
   [4, 41, 4], [4.5, 48, 3.5], [5, 55, 0.5], [5.5, 57, 2.5]
 ]
+// Segment.
 const seg = mu.segment(ps, true, 0, 2)
+// Run HarmAn.
 const ans = mu.harman_forward(seg, mu.chord_templates_pbmin7ths, mu.chord_lookup_pbmin7ths)
 console.log("ans:", ans)
